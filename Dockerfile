843b50cd2add45938da0870369d090f206c7f47d
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2017-01-31 17:15:49 -0200

    Added support for some cli operations and support for dumb-init inside the container.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,20 @@
 from php:7.0-apache
-RUN apt-get update && apt-get install --no-install-recommends git libpq-dev tar sendmail locales libpng-dev libxml2-dev libicu-dev -y && \
+RUN apt-get update && apt-get install --no-install-recommends git libpq-dev tar locales libpng-dev libxml2-dev libicu-dev -y && \
     sed -i 's/# pt_BR.UTF-8/pt_BR.UTF8/' /etc/locale.gen && \
     locale-gen && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* && \
     apt-get clean
 
-RUN docker-php-ext-install pdo_pgsql mysqli pgsql zip gd xmlrpc soap intl && \
+ADD https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64 /usr/bin/dumb-init
+
+RUN chmod +x /usr/bin/dumb-init && \
+    docker-php-ext-install pdo_pgsql mysqli pgsql zip gd xmlrpc soap intl opcache && \
     a2enmod rewrite && a2enmod ssl
+
 COPY files/moodle.conf /etc/apache2/sites-available/default.conf
 COPY files/moodle.php.ini /usr/local/etc/php/conf.d/moodle.ini
-COPY files/docker-entrypoint.sh /docker-entrypoint.sh
+COPY files/docker-entrypoint.sh /entrypoint
+
 WORKDIR /var/www/html
-ENTRYPOINT ["/docker-entrypoint.sh"]
+ENTRYPOINT ["/usr/bin/dumb-init","/entrypoint"]
+CMD ["run"]
--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -1,87 +1,142 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-CONF=/var/www/html/config.php
+run_moodle() {
+    CONF=/var/www/html/config.php
 
-if [ -z ${DB_HOST} ]; then
-    echo "No Default DB Host Provided. Assuming 'db'"
-    DB_HOST="db"
-fi
-
-if [ -z ${DB_PORT} ]; then
-    echo "No Default port for Db Provided. Assuming 5432."
-    DB_PORT=5432
-fi
-
-if [ -z ${DB_USER} ]; then
-    echo "No Default User provided for Db. Assuming moodle"
-    DB_USER="moodle"
-fi
-
-if [ -z ${DB_PASS} ]; then
-    echo "No Default DB Password provided. Assuming moodle"
-    DB_PASS="moodle"
-fi
-
-if [ -z ${DB_NAME} ]; then
-    echo "No default db name provided. Assuming moodle"
-    DB_NAME="moodle"
-fi
-
-if [ -z ${VIRTUAL_HOST} ]; then
-    echo "You dont have provided a virtual host config which is needed. Exiting ..."
-    exit 1
-fi
-
-if [ -z ${MOODLE_VERSION} ]; then
-    echo "No Moodle version provided. Assuming 3.1 ..."
-    MOODLE_VERSION=31
-fi
-
-if [ ! -e $CONF ]; then
-
-    if [ -z ${ADMIN_PASSWORD} ]; then
-        echo "No admin password provided ... Creating one ..."
-        ADMIN_PASSWORD=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;`
-        echo "ADMIN PASSWORD: $ADMIN_PASSWORD"
+    if [ -z ${DB_HOST} ]; then
+        echo "No Default DB Host Provided. Assuming 'db'"
+        DB_HOST="db"
     fi
 
-    echo "Downloading Moodle..."
-    git clone --depth 1 --branch MOODLE_${MOODLE_VERSION}_STABLE https://github.com/moodle/moodle.git .
+    if [ -z ${DB_PORT} ]; then
+        echo "No Default port for Db Provided. Assuming 5432."
+        DB_PORT=5432
+    fi
 
-    touch $CONF
+    if [ -z ${DB_USER} ]; then
+        echo "No Default User provided for Db. Assuming moodle"
+        DB_USER="moodle"
+    fi
 
-cat <<EOT >> $CONF
-<?php
-unset(\$CFG);
-global \$CFG;
-\$CFG = new stdClass();
-\$CFG->dbtype = "pgsql";
-\$CFG->dblibrary = "native";
-\$CFG->dbhost = "$DB_HOST";
-\$CFG->dbname = "$DB_NAME";
-\$CFG->dbuser = "$DB_USER";
-\$CFG->dbpass = "$DB_PASS";
-\$CFG->prefix = 'mdl_';
-\$CFG->dboptions = array(
-    'dbpersist' => false,
-    'dbsocket' => false,
-    'dbport' => "$DB_PORT",
-);
+    if [ -z ${DB_PASS} ]; then
+        echo "No Default DB Password provided. Assuming moodle"
+        DB_PASS="moodle"
+    fi
 
-\$CFG->wwwroot = "$VIRTUAL_HOST";
-\$CFG->dataroot = '/var/www/moodledata';
-\$CFG->admin = 'admin';
-\$CFG->directorypermissions = 0777;
-require_once(dirname(__FILE__) . '/lib/setup.php');
-EOT
+    if [ -z ${DB_NAME} ]; then
+        echo "No default db name provided. Assuming moodle"
+        DB_NAME="moodle"
+    fi
 
-chmod -R 777 /var/www/moodledata
-echo "Moodle Configuration file created. Starting DB Migrations."
-php admin/cli/install_database.php --agree-license --lang=pt_BR --adminemail=ti@raleduc.com.br --adminpass=$ADMIN_PASSWORD
-echo "Database Migrated. Check the frontend to finish the installation proccess."
+    if [ -z ${VIRTUAL_HOST} ]; then
+        echo "You dont have provided a virtual host config which is needed. Exiting ..."
+        exit 1
+    fi
 
-fi
+    if [ -z ${MOODLE_VERSION} ]; then
+        echo "No Moodle version provided. Assuming 3.1 ..."
+        MOODLE_VERSION=31
+    fi
+    
+    if [ -z ${VIRTUAL_PROTO} ]; then
+    	echo "No protocol provided. Assuming http"
+    	VIRTUAL_PROTO=http
+    fi	
 
-exec apache2-foreground
+    if [ ! -e $CONF ]; then
+
+        if [ -z ${ADMIN_PASSWORD} ]; then
+            echo "No admin password provided ... Creating one ..."
+            ADMIN_PASSWORD=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;`
+            echo "ADMIN PASSWORD: $ADMIN_PASSWORD"
+        fi
+
+        echo "Downloading Moodle..."
+        git clone --depth 1 --branch MOODLE_${MOODLE_VERSION}_STABLE https://github.com/moodle/moodle.git .
+
+        touch $CONF
+
+	cat <<-EOF >> $CONF
+		<?php
+		unset(\$CFG);
+		global \$CFG;
+		\$CFG = new stdClass();
+		\$CFG->dbtype = "pgsql";
+		\$CFG->dblibrary = "native";
+		\$CFG->dbhost = "$DB_HOST";
+		\$CFG->dbname = "$DB_NAME";
+		\$CFG->dbuser = "$DB_USER";
+		\$CFG->dbpass = "$DB_PASS";
+		\$CFG->prefix = 'mdl_';
+		\$CFG->dboptions = array(
+			'dbpersist' => false,
+			'dbsocket' => false,
+			'dbport' => "$DB_PORT",
+		);
+
+		\$CFG->wwwroot = "$VIRTUAL_PROTO://$VIRTUAL_HOST";
+		\$CFG->dataroot = '/var/www/moodledata';
+		\$CFG->admin = 'admin';
+		\$CFG->directorypermissions = 0777;
+		require_once(dirname(__FILE__) . '/lib/setup.php');
+	EOF
+
+    chmod -R 777 /var/www/moodledata
+    echo "Moodle Configuration file created. Starting DB Migrations."
+    php admin/cli/install_database.php --agree-license --lang=pt_BR --adminemail=ti@raleduc.com.br --adminpass=$ADMIN_PASSWORD
+    echo "Database Migrated. Check the frontend to finish the installation proccess."
+
+    fi
+    
+    exec apache2-foreground "$@"
+}
+
+maintenance_on() {
+    php /var/www/html/admin/cli/maintenance.php --enable
+    echo "Maintenance is ON"
+}
+
+maintenance_off() {
+    php /var/www/html/admin/cli/maintenance.php --disable
+    echo "Maintenance is OFF"
+}
+
+run_cron() {
+    php /var/www/html/admin/cli/cron.php
+    echo "Running Cron"
+}
+
+update_moodle(){
+    cd /var/www/html
+    git config --global user.email "root@local.host"
+    git config --global user.name "gcavalcante8808"
+    maintenance_on
+    echo "Starting the Update"
+    git stash --include-untracked
+    git pull
+    git stash pop
+    echo "Updates Applied."
+    maintenance_off
+}
+
+case "$@" in 
+    run)
+        run_moodle
+        ;;
+    maintenance_on)
+        maintenance_on
+        ;;
+    maintenance_off)
+        maintenance_off
+        ;;
+    update)
+        update_moodle
+        ;;
+    *)
+        echo "Usage: $0 {run,maintenance_on, maintenance_off, run_cron, update}"
+        exit 1
+        ;;
+esac
+
